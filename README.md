c9a58b078f11eb6cbf538e81fe3f703ef91f4d61
freewind <nowindlee@gmail.com>
2019-03-31 20:21:18 +0800

    demo

--- README.md
+++ README.md
@@ -1,23 +1,9 @@
-TypeScript React "material-ui" Demo
-===================================
+TypeScript React "material-ui" "CircularProgress" Demo
+======================================================
 
-如何使用TypeScript来写"material-ui"
+material-ui奇难用无比的styling，使得我们想要改一下颜色，最后还要靠直接在元素上设置`style`
 
 ```
 npm install
 npm run demo
 ```
-
-It will open <http://localhost:8080/> automatically.
-
-![demo](./images/demo.jpg)
-
-坑
----
-
-### `tsconfig.json`
-
-- "moduleResolution": "node"
-
-这里必须为"node"，否则`import List from '@material-ui/core/List'`会报找不到模块的错误。
-
--- hello.tsx
+++ hello.tsx
@@ -1,45 +1,13 @@
 import * as React from 'react'
+import CircularProgress from "@material-ui/core/CircularProgress";
 
-import {List, ListItem, ListItemText, ListItemIcon, Collapse} from '@material-ui/core'
-import InboxIcon from '@material-ui/icons/Inbox'
-import {ExpandMore, ExpandLess} from '@material-ui/icons'
+class Hello extends React.Component {
 
-class MyList extends React.Component {
-    state = {
-        open: false
-    }
-
-    toggleSublist = () => {
-        this.setState({
-            open: !this.state.open
-        })
-    }
-
-    render() {
-        return <div>
-            <List>
-                <ListItem button onClick={this.toggleSublist}>
-                    <ListItemIcon>
-                        <InboxIcon/>
-                    </ListItemIcon>
-                    <ListItemText primary='Hello'/>
-                    {
-                        this.state.open ? <ExpandLess/> : <ExpandMore/>
-                    }
-                </ListItem>
-                <Collapse in={this.state.open}>
-                    <List>
-                        <ListItem>
-                            <ListItemText primary='typescript'/>
-                        </ListItem>
-                        <ListItem>
-                            <ListItemText primary='material-ui'/>
-                        </ListItem>
-                    </List>
-                </Collapse>
-            </List>
-        </div>
-    }
+  render() {
+    return <div>
+      <CircularProgress style={{color: 'blue'}}/>
+    </div>
+  }
 }
 
-export default MyList
+export default Hello
